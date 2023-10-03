a44b8b1996f1e42af8d3eb9c436734becb93ae7a
christojolly23 <christo.jolly23@students.bhasvic.ac.uk>
2023-10-03 01:37:03 +0100

    finished ticket problem at home

--- Ticket Program/main.c
+++ Ticket Program/main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int ctickets;
-    int atickets;
+    int ctickets, atickets;
+    double cprice, aprice;
     int totalNumPeople;
     float price;
     char day;
     printf("Enter W for Wednesday, S for Saturday or Sunday and B if it's a bank holiday\n");
     scanf("%c", &day);
     printf("Enter the number of child tickets");
-    scanf("%c", &ctickets);
+    scanf("%d", &ctickets);
     printf("Enter the number of adult tickets");
-    scanf("%c", &atickets);
-    if (ctickets > 0) {
+    scanf("%d", &atickets);
+    if (ctickets > 0 && atickets > 0) {
 
         if (day == 'W') {
-            price = 5 * ctickets;
+            aprice = 8.00;
+            cprice = 5.00;
+            price = (cprice * ctickets) + (aprice * atickets);
         }
-        else if (day == 'S') || (day == 'B'){
-            price = 7.5 * ctickets;
+        else if (day == 'S') {
+            aprice = 12.00;
+            cprice = 7.50;
+            price = (cprice * ctickets) + (aprice * atickets);
         }
-}   }
+        else if (day == 'B') {
+            aprice = 14.00;
+            cprice = 7.50;
+            price = (cprice * ctickets) + (aprice * atickets);
+        }
+    printf("%f", price);
+
+    }
+
+}
