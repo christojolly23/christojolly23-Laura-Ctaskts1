80dfc769aeb72317d81cc7a3fae851ce343ff82f
christojolly23 <145435570+christojolly23@users.noreply.github.com>
2023-10-03 10:03:42 +0100

    commit at school

---
+++ CLionProjects/Pathway2maybelesson1/Pathway2Chapter2 Task2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    char operation;
+    char operationSelection;
+    printf("Enter number 1: \n");
+    scanf("d", &num1);
+    printf("Enter number 2: \n");
+    scanf("d", &num2);
+    printf("Enter what operation you would like to carry out (+,-,*,/) eg num1 / num2: \n");
+    scanf("d", &operation);
+    switch(operationSelection)
+    {
+        case '+':
+            printf(num1+num2"%f,%f");
+
+
+    }
+
+    return 0;
+}
